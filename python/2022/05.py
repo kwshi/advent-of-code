6f54ebb9fc799479d80a608462b74c51df792534
Kye Shi <shi.kye@gmail.com>
2022-12-05 21:47:43 -0800

    python: fix and simplify 2022/05

--- python/2022/05.py
+++ python/2022/05.py
@@ -5,9 +5,11 @@ import typing
 
 def parse(stdin: typing.TextIO):
     stack_lines, ins_lines = ks.parse.chunks(stdin)
-    stack: list[list[str]] = [[] for _ in range(max(map(int, stack_lines.pop())))]
+    stack: list[list[str]] = [
+        [] for _ in range(max(map(int, stack_lines.pop().strip().split())))
+    ]
     for line in stack_lines:
-        for s, (_, item, _, _) in zip(stack, ks.iter.grouper(line, 4)):
+        for s, item in zip(stack, line[1::4]):
             if item != " ":
                 s.append(item)
     for s in stack:
@@ -19,13 +21,13 @@ def part1(stdin: typing.TextIO):
     stack, instructions = parse(stdin)
     for n, f, t in instructions:
         for _ in range(n):
-            stack[f - 1].append(stack[t - 1].pop())
+            stack[t - 1].append(stack[f - 1].pop())
     return "".join(s.pop() for s in stack)
 
 
 def part2(stdin: typing.TextIO):
     stack, instructions = parse(stdin)
     for n, f, t in instructions:
-        stack[t - 1].extend(stack[f - 1][n:])
+        stack[t - 1].extend(stack[f - 1][-n:])
         stack[f - 1][-n:] = []
     return "".join(s.pop() for s in stack)
