9ae17640a981d4dd341925a3bb4c283920663df8
Kye Shi <shi.kye@gmail.com>
2022-12-08 22:20:15 -0800

    python/2022/09: refactor aggressively

--- python/2022/09.py
+++ python/2022/09.py
@@ -1,6 +1,7 @@
 # pyright: strict
 from .. import ks
 import typing
+import itertools as it
 
 offset: dict[str, tuple[int, int]] = {
     "L": (-1, 0),
@@ -10,37 +11,29 @@ offset: dict[str, tuple[int, int]] = {
 }
 
 
-def parse(stdin: typing.TextIO) -> typing.Iterator[tuple[tuple[int, int], int]]:
+def parse(stdin: typing.TextIO) -> typing.Iterator[tuple[int, int]]:
     for d, n in ks.parse.lines_pattern(stdin, "%s %d"):
-        yield offset[d], n
+        yield from it.repeat(offset[d], n)
 
 
 def advance(a: ks.coord.Coord, b: ks.coord.Coord) -> ks.coord.Coord:
     return a if a.dist_max(b) <= 1 else a + (b - a).sign()
 
 
-def part1(stdin: typing.TextIO) -> int:
+@ks.func.count_unique
+def part1(stdin: typing.TextIO) -> typing.Iterator[ks.coord.Coord]:
     head = tail = ks.coord.Coord()
-    seen: set[ks.coord.Coord] = {head}
-
-    for d, n in parse(stdin):
-        for _ in range(n):
-            head += d
-            tail = advance(tail, head)
-            seen.add(tail)
-
-    return len(seen)
+    for d in parse(stdin):
+        head += d
+        tail = advance(tail, head)
+        yield tail
 
 
-def part2(stdin: typing.TextIO) -> int:
+@ks.func.count_unique
+def part2(stdin: typing.TextIO) -> typing.Iterator[ks.coord.Coord]:
     rope = [ks.coord.Coord()] * 10
-    seen: set[ks.coord.Coord] = set()
-
-    for d, n in parse(stdin):
-        for _ in range(n):
-            rope[0] += d
-            for i in range(len(rope) - 1):
-                rope[i + 1] = advance(rope[i + 1], rope[i])
-            seen.add(rope[-1])
-
-    return len(seen)
+    for d in parse(stdin):
+        rope[0] += d
+        for i in range(len(rope) - 1):
+            rope[i + 1] = advance(rope[i + 1], rope[i])
+        yield rope[-1]
