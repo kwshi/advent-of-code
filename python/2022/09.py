f0ed84e7ea1a7037d127a044a5e071b323f950a2
Kye Shi <shi.kye@gmail.com>
2022-12-08 22:27:41 -0800

    python/2022/09: hyper-aggressive refactoring (abstract parts 1 & 2)

--- python/2022/09.py
+++ python/2022/09.py
@@ -1,7 +1,6 @@
 # pyright: strict
 from .. import ks
 import typing
-import itertools as it
 
 offset: dict[str, tuple[int, int]] = {
     "L": (-1, 0),
@@ -11,29 +10,23 @@ offset: dict[str, tuple[int, int]] = {
 }
 
 
-def parse(stdin: typing.TextIO) -> typing.Iterator[tuple[int, int]]:
-    for d, n in ks.parse.lines_pattern(stdin, "%s %d"):
-        yield from it.repeat(offset[d], n)
-
-
 def advance(a: ks.coord.Coord, b: ks.coord.Coord) -> ks.coord.Coord:
     return a if a.dist_max(b) <= 1 else a + (b - a).sign()
 
 
-@ks.func.count_unique
-def part1(stdin: typing.TextIO) -> typing.Iterator[ks.coord.Coord]:
-    head = tail = ks.coord.Coord()
-    for d in parse(stdin):
-        head += d
-        tail = advance(tail, head)
-        yield tail
+def solve(l: int) -> typing.Callable[[typing.TextIO], int]:
+    @ks.func.count_unique
+    def run(stdin: typing.TextIO) -> typing.Iterator[ks.coord.Coord]:
+        rope = [ks.coord.Coord()] * l
+        for d, n in ks.parse.lines_pattern(stdin, "%s %d"):
+            for _ in range(n):
+                rope[0] += offset[d]
+                for i in range(l - 1):
+                    rope[i + 1] = advance(rope[i + 1], rope[i])
+                yield rope[-1]
+
+    return run
 
 
-@ks.func.count_unique
-def part2(stdin: typing.TextIO) -> typing.Iterator[ks.coord.Coord]:
-    rope = [ks.coord.Coord()] * 10
-    for d in parse(stdin):
-        rope[0] += d
-        for i in range(len(rope) - 1):
-            rope[i + 1] = advance(rope[i + 1], rope[i])
-        yield rope[-1]
+part1 = solve(2)
+part2 = solve(10)
