76dafc145673f06a403fb84a5440d60a82e36958
Kye W. Shi <shi.kye@gmail.com>
2022-12-13 07:13:23 -0800

    python/2022/13: refactor + pyright strict

--- python/2022/13.py
+++ python/2022/13.py
@@ -1,49 +1,53 @@
+# pyright: strict
+
 from .. import ks
 import typing
 
 import functools as ft
+import ast
 
 
-def parse(stdin: typing.TextIO):
-    for a, b in ks.parse.chunks(stdin):
-        yield eval(a), eval(b)
+Packet = int | list["Packet"]
 
 
-def compare(a, b):
-    match (a, b):
-        case (int(), int()):
+# annoying `as` patterns everywhere because pyright doesn't narrow tuple sub-expression types:
+# <https://github.com/microsoft/pyright/issues/4208#issuecomment-1348755433>
+def compare(a: Packet, b: Packet) -> int:
+    match a, b:
+        case (int() as a, int() as b):
             return a - b
 
-        case (int(), list()):
+        case (int() as a, list() as b):
             return compare([a], b)
 
-        case (list(), int()):
+        case (list() as a, int() as b):
             return compare(a, [b])
 
-        case (list(), list()):
+        case (list() as a, list() as b):
             for x, y in zip(a, b):
                 c = compare(x, y)
-                if c == 0:
-                    continue
-                return c
+                if c != 0:
+                    return c
             return len(a) - len(b)
+
+        case _:
+            # because pyright doesn't understand how tuple pattern-matching exhaustion works
+            assert False
 
 
 @ks.func.sumify
 def part1(stdin: typing.TextIO):
-    for i, (a, b) in enumerate(parse(stdin)):
+    for i, chunk in enumerate(ks.parse.chunks(stdin)):
+        a, b = map(ast.literal_eval, chunk)
         if compare(a, b) < 0:
             yield i + 1
 
 
 def part2(stdin: typing.TextIO):
-    things = []
-    for line in ks.parse.lines(stdin):
-        if not line:
-            continue
-        things.append(eval(line))
-    things.append([[2]])
-    things.append([[6]])
-
+    things = [
+        *map(ast.literal_eval, ks.parse.lines(stdin, ignore_empty=True)),
+        [[2]],
+        [[6]],
+    ]
     things.sort(key=ft.cmp_to_key(compare))
     return (1 + things.index([[2]])) * (1 + things.index([[6]]))
