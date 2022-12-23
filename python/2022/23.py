99337dd02d18c65a526e201fa55971f1bb28fd24
Kye W. Shi <shi.kye@gmail.com>
2022-12-22 21:45:24 -0800

    python/2022/23: clean up

--- python/2022/23.py
+++ python/2022/23.py
@@ -1,18 +1,9 @@
+# pyright: strict
 from .. import ks
 import typing
 
-import math
-import string
-import re
 import itertools as it
-import bisect as bs
-import functools as ft
 import collections as co
-import operator as op
-import dataclasses as dc
-import heapq as hq
-import pprint as pp
-import graphlib as gl
 
 
 def parse(stdin: typing.TextIO):
