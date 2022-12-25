2cd9577a3973d4e9d270575ccd6b1882af5640fd
Kye W. Shi <shi.kye@gmail.com>
2022-12-24 21:42:20 -0800

    python/ks: export some more classes to top-level

--- python/ks/__init__.py
+++ python/ks/__init__.py
@@ -1,6 +1,21 @@
 # pyright: strict
-from . import parse, iter, func, grid, coord, math
+from . import parse, iter, func, grid, coord
 from .uf import Uf
+from .interval import Interval, Dis
+
+from .pt.p2 import P2
+from .pt.p3 import P3
 
 
-__all__ = ["parse", "iter", "func", "grid", "coord", "math", "Uf"]
+__all__ = [
+    "parse",
+    "iter",
+    "func",
+    "grid",
+    "coord",
+    "Uf",
+    "Interval",
+    "Dis",
+    "P2",
+    "P3",
+]
