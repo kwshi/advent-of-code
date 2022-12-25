2dbe5833a2126a668325f340c61bd05d62f9f0ea
Kye W. Shi <shi.kye@gmail.com>
2022-12-25 09:45:12 -0800

    python/ks: rename pt -> point

--- python/ks/__init__.py
+++ python/ks/__init__.py
@@ -3,8 +3,8 @@ from . import parse, iter, func, grid, coord
 from .uf import Uf
 from .interval import Interval, Dis
 
-from .pt.p2 import P2
-from .pt.p3 import P3
+from .point.p2 import P2
+from .point.p3 import P3
 
 
 __all__ = [
