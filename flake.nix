c436c78de5b43049dde80921bd298ee3a55dc6b3
Kye Shi <kiwishi@pm.me>
2023-11-22 12:26:09 -0800

    flake.nix: add various ocaml dependencies

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     let
       system = "x86_64-linux";
       pkgs = inputs.nixpkgs.legacyPackages.${system};
+      ocamlPkgs = pkgs.ocamlPackages;
       pyPkgs = p: with p; [
         z3
         sympy
@@ -15,7 +16,28 @@
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ pup (python311.withPackages pyPkgs) ];
+        nativeBuildInputs = (with ocamlPkgs; [ ocaml findlib dune_3 ocaml-lsp ]);
+
+        buildInputs =
+          (with pkgs; [
+            pup
+            #(python311.withPackages pyPkgs)
+          ])
+          ++ (with ocamlPkgs; [
+            containers
+            alcotest
+            angstrom
+            iter
+            yojson
+            re
+            re2
+            fmt
+            ppx_deriving
+            ppx_derivers
+            qcheck
+            qcheck-alcotest
+          ])
+        ;
       };
     };
 }
